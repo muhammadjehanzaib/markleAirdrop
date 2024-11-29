748ba89ebeeff8532c36c44260e4a423459c6097
muhammadjehanzaib <soulsniper155@gmail.com>
2024-11-29 19:36:16 +0500

    Initial Commit

---
+++ test/unitTest/BananaTokenTest.t.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {BananaToken} from "../../src/BananaToken.sol";
+
+
+contract BananaTokenTest is Test {
+	BananaToken bananaToken;
+
+	function setUp() external {
+		bananaToken = new BananaToken(100000 * 10 **18);
+	}
+	function testTotalSupply() public view {
+		assertEq(bananaToken.totalSupply(), 100000 * 10 ** 18);
+	}
+
+
+}
