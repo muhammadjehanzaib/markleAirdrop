a214aa7a8ec96211e26c12d535b39d092d1bbde3
muhammadjehanzaib <soulsniper155@gmail.com>
2024-12-03 15:13:02 +0500

    Add more packages in order to make testing smooth

--- src/BananaToken.sol
+++ src/BananaToken.sol
@@ -3,11 +3,12 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+contract BananaToken is ERC20, Ownable {
+    constructor() ERC20("BananaToken", "BT") Ownable(msg.sender) {}
 
-contract BananaToken is ERC20 {
-	
-	constructor(uint256 initialSupply) ERC20("BananaToken", "BT") {
-		_mint(msg.sender, initialSupply);	
-	}
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
+    }
 }
